8687457fdd8bf877cc1de7238178d421f065af6a
karus4226 <54826908+karus4226@users.noreply.github.com>
2019-10-23 15:35:46 +0700

    Update file_FindString.py

--- file_FindString.py
+++ file_FindString.py
@@ -1,6 +1,7 @@
 import mmap
 
-with open('test.txt') as f:
-    s = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
-    if s.find('ABC') != -1:
-        print('true')
+def ListFile(input_str, input_file):
+    with open(input_file) as f:
+        s = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
+        if s.find(input_str) != -1:
+            print('true')
